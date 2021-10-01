50f18919fe745c66745cdad85d93508d530e6489
kshiga <kshiga@student.42tokyo.jp>
2021-10-02 02:03:58 +0900

    first st02

---
+++ c06/st02/params_rev.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int c,n;
+	if (argc > 0)
+	{
+		c = argc - 1;
+		while (0 < c)
+		{
+			n = 0;
+			while (argv[c][n] != '\0')
+			{
+				write(1, &argv[c][n], 1);
+				n++;
+			}
+			write(1, "\n", 1);
+			c--;
+		}
+	}
+	return (0);
+}
